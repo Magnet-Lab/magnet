2f776ea7ae8445bc795632e2dd42c488120bfa76
zinu <doglife222@gmail.com>
2023-06-29 23:26:06 +0900

    feat(function): 프로필 버튼

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<div css={mainArea}>
+			<Component {...pageProps} />
+		</div>
+	)
 }
+
+const mainArea = css`
+	max-width: 60vw;
+	min-height: 100vh;
+	margin: 0 auto;
+
+	@media (width < 440px) {
+		max-width: 100vw;
+		background-color: plum;
+	}
+`
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,104 @@
-import { Menu } from "@mui/joy"
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+
+import IconButton from '@mui/joy/IconButton'
+import Menu from '@mui/joy/Menu'
+import MenuItem from '@mui/joy/MenuItem'
+import ListItemDecorator from '@mui/joy/ListItemDecorator'
+import ListDivider from '@mui/joy/ListDivider'
+import Edit from '@mui/icons-material/Edit'
+import DeleteForever from '@mui/icons-material/DeleteForever'
+import Person from '@mui/icons-material/Person'
+import { useRef, useState } from 'react'
+
 export default function Home() {
-  return (
-    <div>
-      <div>
-        indexview
-      </div>
-    </div>
-  )
+	const buttonRef = useRef(null)
+	const [open, setOpen] = useState(false)
+
+	const handleClose = () => {
+		setOpen(false)
+	}
+	return (
+		<div css={mainArea}>
+			<div css={profileArea}>
+				<IconButton
+					ref={buttonRef}
+					id="positioned-demo-button"
+					aria-controls={'positioned-demo-menu'}
+					aria-haspopup="true"
+					aria-expanded={open ? 'true' : undefined}
+					variant="outlined"
+					color="primary"
+					onClick={() => {
+						setOpen(!open)
+					}}
+				>
+					<Person />
+				</IconButton>
+				<Menu
+					id="profile-menu"
+					anchorEl={buttonRef.current}
+					open={open}
+					onClose={handleClose}
+					aria-labelledby="profile-menu-button"
+					placement="bottom"
+				>
+					<MenuItem onClick={handleClose}>
+						<ListItemDecorator>
+							<Edit />
+						</ListItemDecorator>{' '}
+						Edit post
+					</MenuItem>
+					<ListDivider />
+					<MenuItem
+						onClick={handleClose}
+						variant="soft"
+						color="danger"
+					>
+						<ListItemDecorator sx={{ color: 'inherit' }}>
+							<DeleteForever />
+						</ListItemDecorator>{' '}
+						Delete
+					</MenuItem>
+				</Menu>
+			</div>
+			<div css={pivotArea}>
+				<h1>Next.js + MUI + Emotion</h1>
+			</div>
+			<div css = {buttonArea}>
+				<h2>Next.js</h2>
+			</div>
+		</div>
+	)
 }
+
+const mainArea = css`
+	display: flex;
+  flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	width: 60vw;
+  height: 100vh;
+	border: 1px solid #ccc;
+`
+
+const profileArea = css`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 25vh;
+`
+
+const pivotArea = css`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 25vh;
+`
+
+const buttonArea = css`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 25vh;
+`
